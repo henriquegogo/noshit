b859193e07eaf078cefa36aa14fc745d3c80cbee
Henrique Soares <henrique.vasconcelos@hp.com>
2017-10-11 10:09:11 -0300

    Adding more ads selectors

--- noshit.js
+++ noshit.js
@@ -8,7 +8,7 @@ selectors.push('[id^=pagelet_].pagelet'); // Facebook Ads
  'shopping', 'advertis', 'dynad', 'aswift', 'adzerk', 'adcriteo',
  'cto_iframe', 'carbonads', 'teads', 'taboola', 'outbrain', '_OAS_',
  'abrAD_', 'google_ads', 'aep-ads', 'adplugg', 'gwd-ad', 'doubleclick',
- 'googleads', 'googlesyndication']
+ 'googleads', 'googlesyndication', 'ad_container', 'ad-container']
 .forEach(word => {
   selectors.push('iframe[src*="'+word+'"]');
   selectors.push('a[href*="'+word+'"]');
